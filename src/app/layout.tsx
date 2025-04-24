e4260e20fa70ab0e56c3eec60f039ba92ab75765
Kr1m0000 <130426519+Kr1m0000@users.noreply.github.com>
2025-04-24 22:01:17 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "K-Services",
+  description: "Digital solutions services for web and mobile Apps",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="relative">
+      <body className={twMerge(dmSans.className, "antialiased bg-[#EAEEFE]")}>
+        {children}
+      </body>
+    </html>
+  );
+}
